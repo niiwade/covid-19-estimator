67e73f2738adcd82377ee1f607471468efc760bc
niiwade <niilantewade77@gmail.com>
2020-04-13 06:45:44 +0800

    start building

--- src/estimator.py
+++ src/estimator.py
@@ -1,3 +1,7 @@
 def estimator(data):
-  
-  return data
+    input = data 
+return 
+    {     data: {}, // the input data you got
+        impact: {}, // your best case estimation
+        severeImpact: {} // your severe case estimation
+    }
