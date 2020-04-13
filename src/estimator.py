8b945565b947db764ed76895a161a55892b56e1f
niiwade <niilantewade77@gmail.com>
2020-04-14 06:10:30 +0800

    Building Estimator

--- src/estimator.py
+++ src/estimator.py
@@ -1,15 +1,12 @@
-currentlyInfected = [] 
-severeImpact = []
-reportedCases = []
-
-
-def estimator(data = currentlyInfected):
-    data =  ()
-    return  estimator
-
-## Challenge 1
- ## estimateCurrentlyInfected
- ## estimateProjectedInfections
-
-if __name__ == "__main__": 
-    estimator()
+def test_challenge1():
+          for [period_type, challenge] in cases:
+            loop = asyncio.get_event_loop()
+            input = loop.run_until_complete(
+                test_utils.mock_estimation_for(period_type))
+        
+            # nodes from end point
+            data = input["data"]
+            estimate = input["estimate"]
+        
+            output = estimator(data)
+    >       values = test_utils.value_on_fields(output, estimate, challenge)
