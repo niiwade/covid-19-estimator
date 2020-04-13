faaa19eba253d8a2ff694b5bbb6f6a59d2b27d6a
niiwade <niilantewade77@gmail.com>
2020-04-14 05:27:20 +0800

    Building Estimator

--- src/estimator.py
+++ src/estimator.py
@@ -1,7 +1,15 @@
-def estimator(data):
-    input = data 
-return 
-    {     data: {}, // the input data you got
-        impact: {}, // your best case estimation
-        severeImpact: {} // your severe case estimation
-    }
+currentlyInfected = [] 
+severeImpact = []
+reportedCases = []
+
+
+def estimator(data = currentlyInfected):
+    data =  ()
+    return  estimator
+
+## Challenge 1
+ ## estimateCurrentlyInfected
+ ## estimateProjectedInfections
+
+if __name__ == "__main__": 
+    estimator()
